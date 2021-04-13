c63208ab37e5d4ae0998704ac9dd4408095218f7
maria-code341 <talanovama@gmail.com>
2021-04-13 11:29:46 +0200

    Update main.js
    [REFACTOR] changes in project files

--- main.js
+++ main.js
@@ -33,13 +33,26 @@ function tahnoutJmeno() {
     if (jmena.length === 0) {
         return;
     }
-
     // Generujeme náhodný index
-
     // Získáme výherní jméno na patřičném indexu
-
     // Vyřadíme vylosované jméno z osudí
-
     // Výherní jméno si uložíme do pole k ostatním výherním
-    
-}
+
+    // // Generujeme náhodný index
+    // let vyherniIndex = Math.floor(random() * jmena.length);
+    // console.log(vyherniIndex);
+    // // Získáme výherní jméno na patřičném indexu
+    // let vyherniJmeno = jmena[vyherniIndex];
+    // console.log(vyherniJmeno);
+    // // Vyřadíme vylosované jméno z osudí
+    // jmena.splice(vyherniIndex, 1)
+    // console.log(jmena.length);
+    // // Výherní jméno si uložíme do pole k ostatním výherním
+    // tazenaJmena.push(vyherniJmeno);
+
+
+    let vyherce = document.querySelector('#vyherka');
+    vyherce.innerHTML = vyherniJmeno;
+
+    let seznamJmen = document.querySelector('#seznam');
+    seznamJmen.innerHTML = tazenaJmena;
